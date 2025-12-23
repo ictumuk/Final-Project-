691f08f7d41e183b62b6026c04eedf616ffb1764
ictumuk <161337993+ictumuk@users.noreply.github.com>
2025-12-23 10:29:09 +0700

    Delete AI_Model/config.py

--- AI_Model/config.py
+++
@@ -1,9 +0,0 @@
-QDRANT_KEY = 'bLqPbc0p6hz9SHt-58ZJlt9oMJ5XsSFXmIgPIfzG2bA25yf_uawG4A'
-QDRANT_URL = 'https://be552412-5e0f-41db-b895-54d6386eab39.europe-west3-0.gcp.cloud.qdrant.io:6333'
-TAVILY_API_KEY = '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'
-DENSE_MODEL_NAME = 'dangvantuan/vietnamese-embedding'
-COLLECTION_NAME = 'legal_documents'
-GROQ_API_KEY = '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'
-SPARSE_MODEL_NAME = 'Qdrant/bm25'
-LANGSMITH_KEY = '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'
-GENERATIVE_MODEL_NAME = "llama-3.3-70b-versatile"
